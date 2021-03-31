8797444843af1b389dad98d7704c33e7f04992d2
yoshinori maeda <yoshinori@collab-it.net>
2021-04-01 06:08:43 +0900

    Fix: tab width to 4

--- lib/cdk-ecs-stack.ts
+++ lib/cdk-ecs-stack.ts
@@ -4,38 +4,36 @@ import ecs_patterns = require('@aws-cdk/aws-ecs-patterns')
 import * as cdk from '@aws-cdk/core'
 
 export class CdkEcsStack extends cdk.Stack {
-    constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-        super(scope, id, props)
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props)
 
-        // Create VPC and Fargate Cluster
-        // NOTE: Limit AZs to avoid reaching resource quotas
-        const vpc = new ec2.Vpc(this, 'MyVpc', { maxAzs: 2 })
-        const cluster = new ecs.Cluster(this, 'Cluster', { vpc })
+    // Create VPC and Fargate Cluster
+    // NOTE: Limit AZs to avoid reaching resource quotas
+    const vpc = new ec2.Vpc(this, 'MyVpc', { maxAzs: 2 })
+    const cluster = new ecs.Cluster(this, 'Cluster', { vpc })
 
-        const hoge = 0
+    const hoge = 0
 
-        // Instantiate Fargate Service with just cluster and image
-        const fargateService = new ecs_patterns.ApplicationLoadBalancedFargateService(
-            this,
-            'FargateService',
-            {
-                cluster,
-                taskImageOptions: {
-                    image: ecs.ContainerImage.fromRegistry(
-                        'amazon/amazon-ecs-sample'
-                    ),
-                },
-            }
-        )
+    // Instantiate Fargate Service with just cluster and image
+    const fargateService = new ecs_patterns.ApplicationLoadBalancedFargateService(
+      this,
+      'FargateService',
+      {
+        cluster,
+        taskImageOptions: {
+          image: ecs.ContainerImage.fromRegistry('amazon/amazon-ecs-sample'),
+        },
+      }
+    )
 
-        // Setup AutoScaling policy
-        const scaling = fargateService.service.autoScaleTaskCount({
-            maxCapacity: 2,
-        })
-        scaling.scaleOnCpuUtilization('CpuScaling', {
-            targetUtilizationPercent: 50,
-            scaleInCooldown: cdk.Duration.seconds(60),
-            scaleOutCooldown: cdk.Duration.seconds(60),
-        })
-    }
+    // Setup AutoScaling policy
+    const scaling = fargateService.service.autoScaleTaskCount({
+      maxCapacity: 2,
+    })
+    scaling.scaleOnCpuUtilization('CpuScaling', {
+      targetUtilizationPercent: 50,
+      scaleInCooldown: cdk.Duration.seconds(60),
+      scaleOutCooldown: cdk.Duration.seconds(60),
+    })
+  }
 }
--- test/cdk-ecs.test.ts
+++ test/cdk-ecs.test.ts
@@ -3,16 +3,16 @@ import * as cdk from '@aws-cdk/core'
 import * as CdkEcs from '../lib/cdk-ecs-stack'
 
 test('Empty Stack', () => {
-    const app = new cdk.App()
-    // WHEN
-    const stack = new CdkEcs.CdkEcsStack(app, 'MyTestStack')
-    // THEN
-    expectCDK(stack).to(
-        matchTemplate(
-            {
-                Resources: {},
-            },
-            MatchStyle.EXACT
-        )
+  const app = new cdk.App()
+  // WHEN
+  const stack = new CdkEcs.CdkEcsStack(app, 'MyTestStack')
+  // THEN
+  expectCDK(stack).to(
+    matchTemplate(
+      {
+        Resources: {},
+      },
+      MatchStyle.EXACT
     )
+  )
 })
