a27e12be60de8d8c140a1f9aa9e06b7aaed5b296
yoshinori maeda <yoshinori@collab-it.net>
2021-04-01 06:01:18 +0900

    ESLint + Prettier

--- lib/cdk-ecs-stack.ts
+++ lib/cdk-ecs-stack.ts
@@ -1,9 +1,41 @@
-import * as cdk from '@aws-cdk/core';
+import ec2 = require('@aws-cdk/aws-ec2')
+import ecs = require('@aws-cdk/aws-ecs')
+import ecs_patterns = require('@aws-cdk/aws-ecs-patterns')
+import * as cdk from '@aws-cdk/core'
 
 export class CdkEcsStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props)
 
-    // The code that defines your stack goes here
-  }
+        // Create VPC and Fargate Cluster
+        // NOTE: Limit AZs to avoid reaching resource quotas
+        const vpc = new ec2.Vpc(this, 'MyVpc', { maxAzs: 2 })
+        const cluster = new ecs.Cluster(this, 'Cluster', { vpc })
+
+        const hoge = 0
+
+        // Instantiate Fargate Service with just cluster and image
+        const fargateService = new ecs_patterns.ApplicationLoadBalancedFargateService(
+            this,
+            'FargateService',
+            {
+                cluster,
+                taskImageOptions: {
+                    image: ecs.ContainerImage.fromRegistry(
+                        'amazon/amazon-ecs-sample'
+                    ),
+                },
+            }
+        )
+
+        // Setup AutoScaling policy
+        const scaling = fargateService.service.autoScaleTaskCount({
+            maxCapacity: 2,
+        })
+        scaling.scaleOnCpuUtilization('CpuScaling', {
+            targetUtilizationPercent: 50,
+            scaleInCooldown: cdk.Duration.seconds(60),
+            scaleOutCooldown: cdk.Duration.seconds(60),
+        })
+    }
 }
--- test/cdk-ecs.test.ts
+++ test/cdk-ecs.test.ts
@@ -1,13 +1,18 @@
-import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
-import * as cdk from '@aws-cdk/core';
-import * as CdkEcs from '../lib/cdk-ecs-stack';
+import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert'
+import * as cdk from '@aws-cdk/core'
+import * as CdkEcs from '../lib/cdk-ecs-stack'
 
 test('Empty Stack', () => {
-    const app = new cdk.App();
+    const app = new cdk.App()
     // WHEN
-    const stack = new CdkEcs.CdkEcsStack(app, 'MyTestStack');
+    const stack = new CdkEcs.CdkEcsStack(app, 'MyTestStack')
     // THEN
-    expectCDK(stack).to(matchTemplate({
-      "Resources": {}
-    }, MatchStyle.EXACT))
-});
+    expectCDK(stack).to(
+        matchTemplate(
+            {
+                Resources: {},
+            },
+            MatchStyle.EXACT
+        )
+    )
+})
