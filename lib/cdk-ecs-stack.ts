4e6fe136e87ec1ea42434cd8cd3f5468b29497e7
yoshinori maeda <yoshinori@collab-it.net>
2021-04-02 04:43:16 +0900

    ECS Exec 導通確認

--- lib/cdk-ecs-stack.ts
+++ lib/cdk-ecs-stack.ts
@@ -1,13 +1,14 @@
-import ec2 = require('@aws-cdk/aws-ec2');
+import * as ec2 from '@aws-cdk/aws-ec2';
 import * as ecs from '@aws-cdk/aws-ecs';
 import * as ecsPatterns from '@aws-cdk/aws-ecs-patterns';
 import * as cdk from '@aws-cdk/core';
+import * as iam from '@aws-cdk/aws-iam';
 import { SubnetType } from '@aws-cdk/aws-ec2';
+import { Role } from '@aws-cdk/aws-iam';
 
 export class CdkEcsStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-    console.log(this.availabilityZones);
 
     // Create VPC and Fargate Cluster
     // NOTE: Limit AZs to avoid reaching resource quotas
@@ -29,33 +30,71 @@ export class CdkEcsStack extends cdk.Stack {
       containerInsights: true,
     });
 
+    const taskDefinition = new ecs.FargateTaskDefinition(this, 'Task', {
+      cpu: 256,
+      memoryLimitMiB: 512,
+      taskRole: this._createTaskRole(),
+    });
+
+    taskDefinition.addContainer('Nginx', {
+      image: ecs.ContainerImage.fromRegistry('nginx'),
+      portMappings: [{ containerPort: 80 }],
+    });
+
     // Instantiate Fargate Service with just cluster and image
     const fargateService = new ecsPatterns.ApplicationLoadBalancedFargateService(
       this,
       'FargateService',
       {
+        serviceName: 'FargateService',
         cluster,
         assignPublicIp: true,
         publicLoadBalancer: true,
 
-        cpu: 256,
         desiredCount: 1,
-        memoryLimitMiB: 512,
 
-        taskImageOptions: {
-          image: ecs.ContainerImage.fromRegistry('amazon/amazon-ecs-sample'),
-        },
+        taskDefinition,
       }
     );
-    //
-    // // Setup AutoScaling policy
-    // const scaling = fargateService.service.autoScaleTaskCount({
-    //   maxCapacity: 2,
-    // });
-    // scaling.scaleOnCpuUtilization('CpuScaling', {
-    //   targetUtilizationPercent: 50,
-    //   scaleInCooldown: cdk.Duration.seconds(60),
-    //   scaleOutCooldown: cdk.Duration.seconds(60),
-    // });
+
+    // @see https://github.com/pahud/ecs-exec-cdk-demo
+    // Enable ECS Exec
+    const cfnService = fargateService.service.node.findChild(
+      'Service'
+    ) as ecs.CfnService;
+    cfnService.addPropertyOverride('EnableExecuteCommand', true);
+
+    // Setup AutoScaling policy
+    const scaling = fargateService.service.autoScaleTaskCount({
+      maxCapacity: 2,
+    });
+    scaling.scaleOnCpuUtilization('CpuScaling', {
+      targetUtilizationPercent: 50,
+      scaleInCooldown: cdk.Duration.seconds(60),
+      scaleOutCooldown: cdk.Duration.seconds(60),
+    });
+
+    new cdk.CfnOutput(this, 'EcsExecCommand', {
+      value: `ecs_exec_service ${cluster.clusterName} ${fargateService.service.serviceName} ${taskDefinition.defaultContainer?.containerName}`,
+    });
+  }
+
+  private _createTaskRole(): Role {
+    const role = new iam.Role(this, 'TaskRole', {
+      assumedBy: new iam.ServicePrincipal('ecs-tasks.amazonaws.com'),
+    });
+    role.addToPrincipalPolicy(
+      new iam.PolicyStatement({
+        actions: [
+          'ssmmessages:CreateControlChannel',
+          'ssmmessages:CreateDataChannel',
+          'ssmmessages:OpenControlChannel',
+          'ssmmessages:OpenDataChannel',
+        ],
+        resources: ['*'],
+      })
+    );
+
+    return role;
   }
 }
